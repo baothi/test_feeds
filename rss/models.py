977172301270f174c31940d132e3d07f1a2ae57f
baothi <baothi246@gmail.com>
2020-08-14 22:11:44 +0700

    show list index

--- rss/models.py
+++ rss/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=255,blank=True,null=True)
+    description = models.CharField(max_length=1000,blank=True,null=True)
+    website = models.URLField(max_length=255, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True )
+    publish = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- rss/views.py
+++ rss/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import feedparser
+
+from .models import Category, Product
 
 # Create your views here.
 
@@ -11,8 +14,19 @@ def index(request):
         url_text = url_text.replace(',',' ')
         url_text = url_text.split()
         for url in url_text:
+            u = url.split('/')
+            print("11111111111111", u[-1])
+            if not Category.objects.filter(name=u[-1]).exists():
+                categoried = Category(name=u[-1])
+                categoried.save()
+            category = Category.objects.get(name=u[-1])
+            
             feed = feedparser.parse(url) #Parsing XML data
-            print("11111111111111", url)
+            for entries in feed.entries:
+                product = Product(title=entries.title,description=entries.summary,website=entries.link,category=category)
+                product.save()
+        all_products = Product.objects.all()
     else:
         feed = None
-    return render(request, 'rss/reader.html', { 'feed' : feed,})
+        all_products = None
+    return render(request, 'rss/reader.html', { 'feed' : feed,'all_products' : all_products, })
